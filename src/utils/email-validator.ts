b41dd5c5dc5328bee5fc8dcb6fc41ebc2549bb10
Maximiliano Moller <maxmoller@gmail.com>
2021-07-06 20:39:30 -0300

    test: ensure DbAccount call Encrypted with correct password

--- src/utils/email-validator.ts
+++
@@ -1,8 +0,0 @@
-import { EmailValidator } from '../presentation/protocols/email-validator'
-import validator from 'validator'
-
-export class EmailValidatorAdapter implements EmailValidator {
-    isvalid (email: string): boolean {
-        return validator.isEmail(email)
-    }
-}
