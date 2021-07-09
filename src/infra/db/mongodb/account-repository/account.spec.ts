097e2750d4cb764c9f7a62e137ffc65d29cafe6a
Maximiliano Moller <maxmoller@gmail.com>
2021-07-09 15:22:39 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,8 +10,17 @@ describe('Accoubt Mongo Repository', () => {
         await MongoHelper.disconnect()
     })
 
+    beforeEach(async () => {
+        const accountCOllection = MongoHelper.getCollection('accounts')
+        await accountCOllection.deleteMany({})
+    })
+
+    const makeSut = (): AccountMongoRepository => {
+        return new AccountMongoRepository()
+    }
+
     test('Should return an account on sucess', async () => {
-        const sut = new AccountMongoRepository();
+        const sut = makeSut()
         const account = await sut.add({ 
             name: 'any_name',
             email: 'any_email@mail.com',
