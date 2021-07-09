22e1e9f1cb3724f34acc013eed687a172d9e7014
Maximiliano Moller <maxmoller@gmail.com>
2021-07-09 18:19:08 -0300

    feat: ensure contenttyp add

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParse } from '../middlewars/body-parse'
-import {cors} from '../middlewars/cors'
+import {cors } from '../middlewars/cors'
+import { contentType } from '../middlewars/content-type'
 
 export default (app: Express): void => {
     app.use(bodyParse)
     app.use(cors)
+    app.use(contentType)
 }
