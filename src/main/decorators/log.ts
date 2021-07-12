3f25caeb8191f50bd399bfe5acaf6fe0741b4180
Maximiliano Moller <maxmoller@gmail.com>
2021-07-12 18:17:15 -0300

    refactor: move sut creator a factory method

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -9,7 +9,7 @@ export class LogControllerDecorator implements Controller {
     }
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        await this.controller.handle(httpRequest)
+       await this.controller.handle(httpRequest)
        return null
     }
 }
