727bd445e2ffc3457024d515a97dd953621954b3
Maximiliano Moller <maxmoller@gmail.com>
2021-07-21 06:16:51 -0300

    refactor: move Validation to protocols folder

--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,6 +1,6 @@
 import { makeSignupValidation } from './signup-validation'
 import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { Validation } from '../../../presentation/helpers/validators/validation'
+import { Validation } from '../../../presentation/protocols/validation'
 import { CompareFiledValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
 import { EmailValidator } from '../../../presentation/protocols/email-validator'
 import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
