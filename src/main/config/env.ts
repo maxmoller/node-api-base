9031a5e1b9053297458cf02c026623f1b1a37a3d
Maximiliano Moller <maxmoller@gmail.com>
2021-07-24 09:35:00 -0300

    feat: addurldatabase

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api', 
+    mongoUrl: process.env.MONGO_URL || 'mongodb+srv://maxmoller:m@xm0ll3r@cluster0.oeflc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
     port: process.env.PORT || 5050,
     jwtSecret: process.env.JWT_SECRET || '3NHnhT48WuCX'
 }
