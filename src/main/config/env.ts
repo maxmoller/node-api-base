5cd432d0c69261fd75db7e74b0bdd3cf68352437
Maximiliano Moller <maxmoller@gmail.com>
2021-07-25 16:44:15 -0300

    chore: add docker compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: process.env.MONGO_URL || 'mongodb+srv://maxmoller:m@xm0ll3r@cluster0.oeflc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api', 
     port: process.env.PORT || 5050,
     jwtSecret: process.env.JWT_SECRET || '3NHnhT48WuCX'
 }
